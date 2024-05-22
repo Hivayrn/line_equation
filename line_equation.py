12fcddcd6c8ed34bd6a103bc27ceffb7171ab95d
Hivayrn <hivahooshyrn792@gmail.com>
2024-05-22 18:39:01 +0330

    Calculation theta and c in line equation

---
+++ line_equation.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+x = np.array(input("Please enter x values: ").split(), dtype=float)
+y = np.array(input("Please enter y values: ").split(), dtype=float)
+
+def calculate_theta(x, y):
+    x_bar = np.mean(x)
+    y_bar = np.mean(y)
+    numerator = np.sum((x - x_bar) * (y - y_bar))
+    denominator = np.sum((x - x_bar) ** 2)   
+    theta = numerator / denominator
+    return theta
+
+theta_value = calculate_theta(x, y)
+
+def calculate_c(x, y, theta):
+    c = np.mean(y) - theta * np.mean(x)
+    return c
+c_value = calculate_c(x, y, theta_value)
+print ("theta:",theta_value)
+print ("c:",c_value)
